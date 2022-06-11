ed80f3a142cebb70e53747c5d3d3d9db8529057b
luotao <luotaoyeah@gmail.com>
2022-06-11 20:04:38 +0800

    feat(3): adopt uni-ui

--- projects/3/src/pages/index/index.vue
+++ projects/3/src/pages/index/index.vue
@@ -2,7 +2,7 @@
   <view class="content">
     <image class="logo" src="/static/logo.png" />
     <view class="text-area">
-      <text class="title">{{ title }}</text>
+      <uni-tag text='HELLO'></uni-tag>
     </view>
   </view>
 </template>
